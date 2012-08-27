9d90d16497cd5b1eccd3bf54cc32c68b3c2a185d
Will Scott <willscott@gmail.com>
2012-08-27 10:02:53 -0700

    Update to chrome 23

--- src/chrome.js
+++ src/chrome.js
@@ -1,3 +1,3 @@
-chrome.experimental.app.onLaunched.addListener(function() {
+chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('index.html', {top: 0, left: 0, width: 800, height: 600});
 });
